502348fbfacf1d17079c7263222fc41f9c863470
Curtis D'Alves <curtis.dalves@gmail.com>
2021-02-03 22:04:37 -0500

    pre-week04

---
+++ assessments/intended-learning-outcomes/auto/Intended-Learning-Outcomes.el
@@ -0,0 +1,38 @@
+(TeX-add-style-hook
+ "Intended-Learning-Outcomes"
+ (lambda ()
+   (setq TeX-command-extra-options
+         "\"-shell-escape\"")
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
+   (add-to-list 'LaTeX-verbatim-environments-local "minted")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art10"
+    "inputenc"
+    "fontenc"
+    "graphicx"
+    "longtable"
+    "hyperref"
+    "natbib"
+    "amssymb"
+    "amsmath"
+    "geometry"
+    "grffile"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "textcomp"
+    "capt-of"
+    "minted"))
+ :latex)
+
