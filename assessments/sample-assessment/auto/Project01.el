bc277cbe2bd7094618a1166c2872e07ddeae7a45
Curtis D'Alves <curtis.dalves@gmail.com>
2021-03-29 21:26:47 -0400

    working on sample-assessment, reformatted and added README rubric

--- assessments/sample-assessment/auto/Project01.el
+++ assessments/sample-assessment/auto/Project01.el
@@ -3,63 +3,64 @@
  (lambda ()
    (setq TeX-command-extra-options
          "\"-shell-escape\"")
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem") ("xcolor" "dvipsnames")))
    (add-to-list 'LaTeX-verbatim-environments-local "minted")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "article"
-    "art10"
+    "art11"
     "inputenc"
     "fontenc"
     "graphicx"
-    "longtable"
-    "hyperref"
-    "natbib"
-    "amssymb"
-    "amsmath"
-    "geometry"
     "grffile"
+    "longtable"
     "wrapfig"
     "rotating"
     "ulem"
+    "amsmath"
     "textcomp"
+    "amssymb"
     "capt-of"
+    "hyperref"
     "minted"
     "xcolor"
     "xcolor-solarized"
-    "sectsty")
+    "sectsty"
+    "geometry")
    (LaTeX-add-labels
-    "sec:org4d2f8e8"
-    "sec:orgff93b8d"
-    "sec:org8b1c30b"
-    "sec:org1a66712"
-    "sec:orgd2f559f"
-    "sec:org5ec3048"
-    "sec:org20408d2"
-    "sec:org6a76171"
-    "sec:orgcf154f9"
-    "sec:org30a98f7"
-    "sec:org65f627a"
-    "sec:org212330d"
-    "sec:org7bca8d6"
-    "sec:org50347ca"
-    "sec:org5b66760"
-    "sec:orga415165"
-    "sec:org5b52355"
-    "sec:orgcde88c8"
-    "sec:org6869260"
-    "sec:org546e8f6"
-    "sec:orgd861ef6"
-    "sec:org6ac54dd"
-    "sec:org4f43c78"))
+    "sec:orgd4cb7b2"
+    "sec:org2c97204"
+    "sec:org22abc0e"
+    "sec:org77629af"
+    "sec:org045dd9e"
+    "sec:org55ef7d8"
+    "sec:orge4abc2f"
+    "sec:orge811c1c"
+    "sec:org0e67195"
+    "sec:org0b7a4b5"
+    "sec:orgff862cb"
+    "sec:orgd691c48"
+    "sec:org2a503cd"
+    "sec:org87618e6"
+    "sec:org69118c6"
+    "sec:org63cda52"
+    "sec:org3325a36"
+    "sec:org1edbd79"
+    "sec:org04c3565"
+    "sec:org6a6740d"
+    "sec:org22c0153"
+    "sec:orgdb0ed05"
+    "sec:org9099349"))
  :latex)
 
